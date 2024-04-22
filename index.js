43368c692d357a1014ba0367c0498d2679bcbf23
Carola <111399169+MCarolG@users.noreply.github.com>
2024-04-21 20:46:53 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("'Hola mundo")
+
+var colors = require('colors');
+
+console.log('Mi nombre es: Carolina'.rainbow);
+
+let sum = 3 + 4;
+console.log(sum);
+
+let num1 = 2;
+let num2 = 3;
+
+let resultado = num1 + num2;
+console.log("El resultado de la suma es: ".green + resultado);
+
+let saludo = "Hola ";
+let nombre = "Carola";
+let saludofinal = saludo + nombre;
+console.log(saludofinal.blue);
